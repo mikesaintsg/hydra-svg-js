19d1297f65f3793b8ab6ff458d7800fba7c4e832
mikesaintsg <45922630+mikesaintsg@users.noreply.github.com>
2021-03-04 17:43:55 -0500

    remove dist before compile

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,17 @@
+const fs = require('fs');
 const {src, dest} = require('gulp');
 const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
 
 const babelConfig = require("./babel.config.js")
 
-exports.default = function () {
+fs.rmdirSync('dist', { recursive: true });
+
+exports.default = function() {
 	return src('src/**/*.js')
 		.pipe(babel(babelConfig))
-		.pipe(src('dist/**/**.js'))
+		.pipe(dest('dist/'))
+		.pipe(src('dist/**/*.js'))
 		.pipe(uglify())
 		.pipe(dest('dist/'));
 }

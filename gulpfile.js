b0b8ef820a2544090649aebaf2b6749a419adcfd
mikesaintsg <45922630+mikesaintsg@users.noreply.github.com>
2021-03-05 08:55:27 -0500

    updated gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,30 @@
 const fs = require('fs');
 const {src, dest} = require('gulp');
-const babel = require('gulp-babel');
-const uglify = require('gulp-uglify');
 
-const babelConfig = require("./babel.config.js")
+const rollup = require('gulp-better-rollup')
+const sourcemaps = require('gulp-sourcemaps')
+const uglify = require('gulp-uglify');
+const sizereport = require('gulp-sizereport')
+
+const babel = require('rollup-plugin-babel')
+const commonjs = require('rollup-plugin-commonjs');
+
+const babelOptions = require('./babel.config');
 
 fs.rmdirSync('dist', { recursive: true });
 
 exports.default = function() {
-	return src('src/**/*.js')
-		.pipe(babel(babelConfig))
+	return src('src/main.js')
+		.pipe(sourcemaps.init())
+		.pipe(rollup(
+			{plugins: [commonjs(),babel(babelOptions)]}, 'cjs'))
+		.pipe(sourcemaps.write())
 		.pipe(dest('dist/'))
 		.pipe(src('dist/**/*.js'))
 		.pipe(uglify())
-		.pipe(dest('dist/'));
+		.pipe(dest('dist/'))
+		.pipe(src('dist/**/*.js'))
+		.pipe(sizereport({
+			gzip: true
+		}));
 }

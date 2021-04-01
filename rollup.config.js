d71b2e6ab88a44da6641c21c49b749ae87eb65f6
mikesaintsg <45922630+mikesaintsg@users.noreply.github.com>
2021-04-01 11:04:52 -0400

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -5,23 +5,29 @@ import babel from '@rollup/plugin-babel'
 import {terser} from 'rollup-plugin-terser'
 import filesize from "rollup-plugin-filesize";
 
-export default {
-	input: 'src/main.js',
-	output: {
-		file: 'dist/main.js',
-		format: 'cjs',
-		exports: 'named'
-	},
+const defaults = {
 	external: [/@babel\/runtime/],
 	plugins: [
 		stripCode({
 			start_comment: 'TEST-ONLY:START',
 			end_comment: 'TEST-ONLY:END'
 		}),
-		resolve(),
+		resolve({ extensions: ['.js'] }),
 		commonjs(),
 		babel({babelHelpers: 'runtime'}),
 		terser(),
 		filesize()
 	]
-};
+}
+
+const cjs = {
+	input: 'src/main.js',
+	output: {
+		file: 'dist/main.js',
+		format: 'cjs',
+		exports: 'named'
+	},
+	...defaults
+}
+
+export default [cjs];

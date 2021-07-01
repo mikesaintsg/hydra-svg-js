550be30c202c01495d665b908aaaf39a1db898b2
mikesaintg11 <mikesaintsg11@gmail.com>
2021-07-01 21:05:37 +0000

    improve and better styles for output

--- config/generatePacks.js
+++ config/generatePacks.js
@@ -1,19 +1,22 @@
 import fs from "fs";
 import generate from "../src/generate.js";
+
 import colors from 'colors';
 
 (async () => {
 	const packs = await fs.promises.readdir('src/packs');
 
-	process.stdout.write("\n" + colors.green(`Generating Packs...`) + "\n");
+	process.stdout.write("\n" + colors.blue(`Generating Packs...`) + "\n");
 
 	await Promise.all(packs.map(pack => {
 		generate({
 			input: `src/packs/${pack}`,
 			output: 'dist/packs',
 			name: `${pack}.json`
-		}).then(()=>{
-			process.stdout.write(colors.blue(`Generated: ${pack}`) + "\n");
+		}).then(() => {
+			process.stdout.clearLine(0);
+			process.stdout.cursorTo(0)
+			process.stdout.write(colors.green(`Generated: ${pack}`) + "\n");
 		});
 	}))
 })();

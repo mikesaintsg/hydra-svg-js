aa3d0d219255c6b65e0d0f3af05d1ae436539cb0
mikesaintg11 <mikesaintsg11@gmail.com>
2021-07-02 13:14:45 +0000

    Created function uniqueArray to filter down to unique items

---
+++ src/utils/uniqueArray.js
@@ -0,0 +1,12 @@
+import forEach from "./forEach";
+
+export default function (array) {
+	const uniqueArray = [];
+	const sortedArray = array.sort()
+
+	forEach(sortedArray, (item, index) => {
+		if(item !== sortedArray[index - 1]) uniqueArray.push(item);
+	})
+
+	return uniqueArray;
+}

d939168fab25d13d837cdd6e8b9dc6f876e9abec
mikesaintg11 <mikesaintsg11@gmail.com>
2021-07-12 12:56:32 +0000

    Refactor Hydrate Test

--- test/unit/hydrate.test.js
+++ test/unit/hydrate.test.js
@@ -5,47 +5,52 @@ import hydrate from "../../src/hydrate.js";
 
 const packs = {
 	"feather": {
-		"activity": {
+		"hash": {
 			"xmlns": "http://www.w3.org/2000/svg",
+			"width": "24",
+			"height": "24",
 			"viewBox": "0 0 24 24",
 			"fill": "none",
 			"stroke": "currentColor",
 			"stroke-width": "2",
 			"stroke-linecap": "round",
 			"stroke-linejoin": "round",
-			"class": "feather feather-activity",
-			"path": [{"d": "M22 12h-4l-3 9L9 3l-3 9H2"}]
+			"class": "feather feather-hash",
+			"line": [{"x1": "4", "y1": "9", "x2": "20", "y2": "9"}, {
+				"x1": "4",
+				"y1": "15",
+				"x2": "20",
+				"y2": "15"
+			}, {"x1": "10", "y1": "3", "x2": "8", "y2": "21"}, {"x1": "16", "y1": "3", "x2": "14", "y2": "21"}]
 		}
 	}
 }
 
-const iconObject = packs.feather.activity;
+const iconObject = packs.feather.hash;
+
+beforeEach(() => {
+	document.body.innerHTML = `<svg pack="feather" icon="hash" id="target"></svg>`
+})
 
 test("will set attributes", () => {
-	document.body.innerHTML = `<svg pack="feather" icon="activity" id="target"></svg>`
+	const target = getById("target");
 
-	const initAttributes = ['pack', 'icon', 'id'];
-
-	const svgs = [].slice.call(document.getElementsByTagName('svg'))
-
-	hydrate(packs, svgs, {observe: false})
+	hydrate(packs, [target], {observe: false})
 
 	const expected = objectFilter(iconObject, value => !Array.isArray(value));
 
-	const actual = attributesFilteredObject(getById('target'),
-		name => !initAttributes.includes(name));
+	const actual = attributesFilteredObject(target,
+		name => !['pack', 'icon', 'id'].includes(name));
 
 	expect(actual).toStrictEqual(expected);
 })
 
 test("will merge existing attributes", () => {
-	document.body.innerHTML = `<svg pack="feather" icon="activity" id="target" class="existing"></svg>`
+	const target = getById("target");
 
-	const svgs = [].slice.call(document.getElementsByTagName('svg'))
+	target.setAttribute("class", "existing");
 
-	hydrate(packs, svgs, {observe: false})
-
-	const target = document.getElementById('target');
+	hydrate(packs, [target], {observe: false})
 
 	const expected = `existing ${iconObject.class}`
 	const actual = target.getAttribute("class")
